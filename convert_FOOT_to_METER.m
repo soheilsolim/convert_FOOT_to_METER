c308a7d9f114cd02035d9e8e97e7b56da8d93ff2
soheil <140661028+soheilsolim@users.noreply.github.com>
2024-03-14 10:49:46 -0700

    Add files via upload

---
+++ convert_FOOT_to_METER.m
@@ -0,0 +1,7 @@
+%soheil mansouri (electrical engineering)
+%Script file to convert foot to meter
+%
+FOOT=input('number in FOOT:');  %give FOOT 
+METER=(FOOT*0.3048);
+disp(['number in METER=',num2str(METER)])  %get METER
+%stop
